1d1b01e451d3ed54f1ee30a7e1dfe4660f3de8af
Owen Buckley <owenbuckley13@gmail.com>
2021-07-21 12:19:12 -0400

    update thresholds

--- nyc.config.js
+++ nyc.config.js
@@ -15,10 +15,10 @@ module.exports = {
 
   checkCoverage: true,
 
-  statements: 85,
-  branches: 75,
+  statements: 80,
+  branches: 70,
   functions: 80,
-  lines: 85,
+  lines: 80,
 
   watermarks: {
     statements: [75, 85],
