388ae856a8f92f531aeece34154edd701e6f0bfa
Owen Buckley <owenbuckley13@gmail.com>
2025-05-09 21:04:17 -0400

    use rm.Sync

--- src/lib/runner.js
+++ src/lib/runner.js
@@ -104,7 +104,7 @@ class Runner {
             : file;
 
           if (fs.lstatSync(deletePath).isDirectory()) {
-            fs.rmdirSync(deletePath, { recursive: true });
+            fs.rmSync(deletePath, { recursive: true });
           } else {
             fs.unlinkSync(deletePath);
           }
