6e1f3b3cf973c380e9ab403e171d5b181c3c84c2
Owen Buckley <owenbuckley13@gmail.com>
2025-05-22 08:31:48 -0400

    add test case for args array

---
+++ test/cases/runner-cli-args-array/runner.cli-args-array.spec.js
@@ -0,0 +1,124 @@
+/*
+ * Use Case
+ * Runs the CLI fixture that copies dot files from fixtures directory and passing args as an array.  Based
+ * on copy-dots: https://github.com/thescientist13/copy-dots
+ *
+ * User Result
+ * Should run cli.js runCommand successfully with setup, runCommand and teardown functions called
+ *
+ * runCommand
+ * runCommand('test/fixtures/cli', 'test/fixtures')
+ *
+ */
+import chai from 'chai';
+import fs from 'fs-extra';
+import path from 'path';
+import { Runner } from '../../../src/index.js';
+import { fileURLToPath, URL } from 'url';
+
+const expect = chai.expect;
+
+describe('CLI Fixture', function() {
+  const outputPath = fileURLToPath(new URL('./output', import.meta.url));
+  const fixturesPath = path.join(process.cwd(), 'test/fixtures');
+  const setupFiles = [{
+    source: path.join(process.cwd(), 'node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle.js'),
+    destination: path.join(outputPath, 'webcomponents-bundle.js')
+  }];
+
+  describe('default options with relative path with args passed as an array', function() {
+    let runner;
+
+    before(function() {
+      runner = new Runner();
+      runner.setup(outputPath);
+      runner.runCommand(
+        `${fixturesPath}/cli.js`, // binPath
+        [fixturesPath] // args
+      );
+    });
+
+    it('should have created the output folder', function() {
+      const exists = fs.existsSync(outputPath);
+
+      expect(exists).to.be.equal(true);
+    });
+
+    it('should only copy 3 files', function() {
+      const files = fs.readdirSync(outputPath);
+
+      expect(files.length).to.be.equal(3);
+    });
+
+    it('should have an .editorconfig file', function() {
+      expect(fs.existsSync(`${outputPath}/.editorconfig`)).to.be.equal(true);
+    });
+
+    it('should have an .eslintrc file', function() {
+      expect(fs.existsSync(`${outputPath}/.eslintrc.cjs`)).to.be.equal(true);
+    });
+
+    it('should have .mocharc file', function() {
+      expect(fs.existsSync(`${outputPath}/.mocharc.cjs`)).to.be.equal(true);
+    });
+
+    it('should delete the output directory when told', function() {
+      runner.teardown([outputPath]);
+
+      expect(fs.existsSync(outputPath)).to.be.equal(false);
+    });
+  });
+
+  describe('setup with setupFiles', function() {
+    let runner;
+
+    before(function() {
+      runner = new Runner();
+      runner.setup(outputPath, setupFiles);
+      runner.runCommand(
+        `${fixturesPath}/cli.js`, // binPath
+        fixturesPath // args
+      );
+    });
+
+    it('should have created the output folder', function() {
+      const exists = fs.existsSync(outputPath);
+
+      expect(exists).to.be.equal(true);
+    });
+
+    it('should only copy 4 files', function() {
+      const files = fs.readdirSync(outputPath);
+
+      expect(files.length).to.be.equal(4);
+    });
+
+    it('should have an .editorconfig file', function() {
+      expect(fs.existsSync(`${outputPath}/.editorconfig`)).to.be.equal(true);
+    });
+
+    it('should have an .eslintrc file', function() {
+      expect(fs.existsSync(`${outputPath}/.eslintrc.cjs`)).to.be.equal(true);
+    });
+
+    it('should have .mocharc file', function() {
+      expect(fs.existsSync(`${outputPath}/.mocharc.cjs`)).to.be.equal(true);
+    });
+
+    it('should have webcomponents-bundle.js file', function() {
+      expect(fs.existsSync(`${outputPath}/webcomponents-bundle.js`)).to.be.equal(true);
+    });
+
+    it('should delete the setup file we provided', function() {
+      const setupFile = path.join(outputPath, 'webcomponents-bundle.js');
+      runner.teardown();
+
+      expect(fs.existsSync(setupFile)).to.be.equal(false);
+      expect(fs.existsSync(outputPath)).to.be.equal(true);
+
+      // cleanup everything
+      runner.teardown([outputPath]);
+      expect(fs.existsSync(outputPath)).to.be.equal(false);
+    });
+  });
+});
