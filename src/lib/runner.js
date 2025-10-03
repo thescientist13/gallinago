7d00ce1bdda32ae1f20487d8d940107bcf4744b6
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2025-10-03 14:13:48 -0700

    fix: namespace node imports

--- src/lib/runner.js
+++ src/lib/runner.js
@@ -1,7 +1,7 @@
-import fs from 'fs';
-import os from 'os';
-import path from 'path';
-import { spawn, spawnSync } from 'child_process';
+import fs from 'node:fs/promises';
+import os from 'node:os';
+import path from 'node:path';
+import { spawn } from 'node:child_process';
 
 class Runner {
   #rootDir;
