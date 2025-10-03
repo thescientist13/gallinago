3228d9029812881887e41ed675cf6718797b07f0
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2025-10-03 14:13:19 -0700

    feat: convert runner to async only

--- src/lib/runner.js
+++ src/lib/runner.js
@@ -4,124 +4,203 @@ import path from 'path';
 import { spawn, spawnSync } from 'child_process';
 
 class Runner {
-  constructor(enableStdOut = false, forwardParentArgs = false) {
-    this.enableStdOut = enableStdOut; // debugging tests
-    this.forwardParentArgs = forwardParentArgs;
-    this.setupFiles = [];
-    this.childProcess = null;
+  #rootDir;
+  #setupFiles = [];
+  #childProcess = null;
+
+  #enableDebug = false;
+  #forwardParentArgs = false;
+
+  // JS strings can be up to 2^53 - 1 in length,
+  // so no need to worry about overflow
+  // https://262.ecma-international.org/16.0/index.html#sec-ecmascript-language-types-string-type
+  #stdOutBuffer = '';
+  #stdErrBuffer = '';
+
+  #stdOutSubscribers = [];
+  #stdErrSubscribers = [];
+
+  constructor(enableDebug = false, forwardParentArgs = false) {
+    this.#enableDebug = enableDebug; // debugging tests
+    this.#forwardParentArgs = forwardParentArgs;
   }
 
-  setup(rootDir, setupFiles = [], options = { create: true }) {
-    this.setupFiles = setupFiles;
+  async setup(rootDir, setupFiles = null, options = { create: true }) {
+    this.#setRootDir(rootDir);
+    this.#setSetupFiles(setupFiles);
 
-    return new Promise((resolve, reject) => {
-      if (path.isAbsolute(rootDir)) {
-        this.rootDir = rootDir;
-
-        if (options.create) {
-          fs.mkdirSync(this.rootDir, { recursive: true });
+    if (options.create) {
+      try {
+        await fs.mkdir(this.#rootDir, { recursive: true });
+      } catch (err) {
+        if (err.message?.startsWith('EEXIST: file already exists')) {
+          // Do nothing
+        } else {
+          throw err;
         }
-
-        if (setupFiles.length > 0) {
-          setupFiles.forEach((file) => {
-            fs.mkdirSync(path.dirname(file.destination), { recursive: true });
-            fs.copyFileSync(file.source, file.destination);
-          });
-        }
-
-        resolve();
-      } else {
-        reject('Error: rootDir is not an absolute path');
       }
-    });
+    }
+
+    for (const file of setupFiles ?? []) {
+      await fs.mkdir(path.dirname(file.destination), { recursive: true });
+      await fs.copyFile(file.source, file.destination);
+    }
   }
 
-  runCommand(binPath, args, options = {}) {
-    return new Promise((resolve, reject) => {
-      const executable = 'node';
-      const isWindows = os.platform() === 'win32';
-      const cliPath = binPath;
-      const forwardArgs = this.forwardParentArgs ? process.execArgv : [];
-      const finalArgs = [...forwardArgs, cliPath];
-      const spawnAction = options.async ? spawn : spawnSync;
-      let err = '';
+  async runCommand(binPath, args, options = { background: false }) {
+    const executable = 'node';
+    const isWindows = os.platform() === 'win32';
+    const forwardArgs = this.#forwardParentArgs ? process.execArgv : [];
+    const finalArgs = [...forwardArgs, binPath];
 
-      if (Array.isArray(args)) {
-        finalArgs.push(...args);
-      } else if (typeof args === 'string') {
-        finalArgs.push(args);
-      }
+    if (Array.isArray(args)) {
+      finalArgs.push(...args);
+    } else if (typeof args === 'string') {
+      finalArgs.push(args);
+    }
 
-      if (!fs.existsSync(binPath)) {
-        reject(`Error: Cannot find path ${binPath}`);
-      }
+    await this.#throwIfBinNotExist(binPath);
 
-      this.childProcess = spawnAction(executable, finalArgs, {
-        cwd: this.rootDir,
-        shell: false,
-        detached: !isWindows,
-        stdio: this.enableStdOut ? 'inherit' : null
-      });
-
-      this.childProcess.on('close', code => {
-        if (err !== '' && code && code !== 0) {
-          reject(err);
-          return;
-        }
-        resolve();
-      });
-
-      this.childProcess.stderr.on('data', (data) => {
-        err = data.toString('utf8');
-        if (this.enableStdOut) {
-          console.error(err);
-        }
-      });
-
-      this.childProcess.stdout.on('data', (data) => {
-        if (this.enableStdOut) {
-          console.log(data.toString('utf8'));
-        }
-      });
+    this.#childProcess = spawn(executable, finalArgs, {
+      cwd: this.#rootDir,
+      shell: false,
+      detached: !isWindows,
     });
+
+    this.#childProcess.stdout.on('data', this.#handleStdOut.bind(this));
+    this.#childProcess.stderr.on('data', this.#handleStdErr.bind(this));
+
+    if (options.background) {
+      // Resolve immediately so caller can start processing outputs
+      return new Promise((resolve, reject) => {
+        this.#childProcess.on('spawn', resolve);
+        this.#childProcess.on('error', reject);
+      });
+    } else {
+      // Resolve when process exits
+      return new Promise((resolve, reject) => {
+        this.#childProcess.on('error', reject);
+        this.#childProcess.on('close', (code) => {
+          if (code) {
+            reject(
+              new Error(`Error: Child process exited with error code ${code}.`)
+            );
+          } else {
+            resolve();
+          }
+        });
+      });
+    }
+  }
+
+  getStdOut() {
+    return this.#stdOutBuffer;
+  }
+
+  getStdErr() {
+    return this.#stdErrBuffer;
+  }
+
+  onStdOut(callback, options = { replay: true }) {
+    if (options.replay && this.#stdOutBuffer.length > 0) {
+      callback(this.#stdOutBuffer);
+    }
+
+    this.#stdOutSubscribers.push(callback);
+  }
+
+  onStdErr(callback, options = { replay: true }) {
+    if (options.replay && this.#stdErrBuffer.length > 0) {
+      callback(this.#stdErrBuffer);
+    }
+
+    this.#stdErrSubscribers.push(callback);
   }
 
   stopCommand() {
-    return new Promise((resolve) => {
-      if (this.childProcess) {
-        this.childProcess.kill();
-        resolve();
-      }
-    });
+    if (!this.#childProcess) {
+      return;
+    }
+
+    if (!this.#childProcess.kill()) {
+      const processID = this.#childProcess.pid;
+      throw new Error(
+        `Error: Failed to kill child process (PID: ${processID})`
+      );
+    }
   }
 
-  teardown(additionalFiles = []) {
-    return new Promise((resolve, reject) => {
-      try {
-        (this.setupFiles || []).concat(additionalFiles).forEach((file) => {
-          const deletePath = file.destination
-            ? file.destination
-            : file;
+  async teardown(additionalFiles = []) {
+    try {
+      for (const file of [...this.#setupFiles, ...additionalFiles]) {
+        const deletePath = file.destination ? file.destination : file;
 
-          if (fs.existsSync(deletePath)) {
-            if (fs.lstatSync(deletePath).isDirectory()) {
-              fs.rmSync(deletePath, { recursive: true });
-            } else {
-              fs.unlinkSync(deletePath);
-            }
-          }
-        });
-
-        this.setupFiles = [];
-        resolve();
-      } catch (err) {
-        console.log(err);
-        reject(err);
+        await fs.rm(deletePath, { recursive: true });
       }
-    });
+
+      this.#setupFiles = [];
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  }
+
+  #handleStdOut(data) {
+    const text = data.toString('utf8');
+    this.#stdOutBuffer += text;
+
+    if (this.#enableDebug) {
+      console.log(text);
+    }
+
+    this.#stdOutSubscribers.forEach((callback) => callback(text));
+  }
+
+  #handleStdErr(data) {
+    const text = data.toString('utf8');
+    this.#stdErrBuffer += text;
+
+    if (this.#enableDebug) {
+      console.error(text);
+    }
+
+    this.#stdErrSubscribers.forEach((callback) => callback(text));
+  }
+
+  #setRootDir(rootDir) {
+    if (!path.isAbsolute(rootDir)) {
+      throw new Error('Error: rootDir is not an absolute path');
+    }
+
+    this.#rootDir = rootDir;
+  }
+
+  #setSetupFiles(setupFiles) {
+    setupFiles ??= [];
+
+    if (
+      !Array.isArray(setupFiles) ||
+      !setupFiles.every((file) => 'source' in file && 'destination' in file)
+    ) {
+      throw new Error(
+        'SetupFiles must be an array with source and destination'
+      );
+    }
+
+    this.#setupFiles = setupFiles;
+  }
+
+  async #throwIfBinNotExist(binPath) {
+    try {
+      await fs.access(binPath);
+    } catch (err) {
+      if (err.message.startsWith('ENOENT: no such file or directory')) {
+        throw new Error(`Error: Cannot find path ${binPath}`);
+      } else {
+        throw err;
+      }
+    }
   }
 }
 
-export {
-  Runner
-};
+export { Runner };
