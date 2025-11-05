2d577729de07347ddda7742af4dea32adb898820
Owen Buckley <owenbuckley13@gmail.com>
2025-11-04 22:07:29 -0500

    update runCommand types

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,8 +4,11 @@ interface RunnerConstructor {
 
 export interface RunnerInterface {
   setup(rootDir: string, setupFiles?: string[], options?: { create?: boolean }): Promise<void>;
-  runCommand(binPath: string, args?: string | string[]): Promise<void>;
+  
+  runCommand(binPath: string, args?: string | string[], options?: { onStdOut: (message:string) => void }): Promise<void>;
+  
   stopCommand(): Promise<void>;
+  
   teardown(additionalFiles?: string[]): Promise<void>;
 }
 
